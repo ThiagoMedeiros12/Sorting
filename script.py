c55b9826f4761232591fc0c9721e6af5a0be9c49
Thiago Medeiros <thia.med@hotmail.com>
2026-04-24 08:32:44 -0300

    configurado uv

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import random
 
 
 def num_generate():
-    return random.randint(1,100)
+    return random.randint(1, 100)
+
 
 def generate_100():
     lista_100 = []
@@ -10,22 +11,23 @@ def generate_100():
         new_value = num_generate()
         if new_value not in lista_100:
             lista_100.append(new_value)
-    return  lista_100
+    return lista_100
+
 
 def bubble_sort(lista):
     while True:
         swaped = False
-        for i in range(0,len(lista)-1):
-            if lista[i] > lista[i+1]:
+        for i in range(0, len(lista) - 1):
+            if lista[i] > lista[i + 1]:
                 aux = lista[i]
-                lista[i] = lista [i + 1]
+                lista[i] = lista[i + 1]
                 lista[i + 1] = aux
                 swaped = True
         if swaped == False:
             return lista
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     x = generate_100()
     print(x)
     print(bubble_sort(x))
